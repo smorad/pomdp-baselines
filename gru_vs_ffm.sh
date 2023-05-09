8405bc7024a9e3bf17886dc087c90425438a93c3
Steven Morad <sm2558@cl.cam.ac.uk>
2023-05-09 09:23:47 +0100

    small fixes

--- gru_vs_ffm.sh
+++ gru_vs_ffm.sh
@@ -18,6 +18,24 @@ CONFIGS_FFM_V=(configs/pomdp/hopper_blt/v/ffm.yml
 	configs/pomdp/ant_blt/v/ffm.yml
 	configs/pomdp/cheetah_blt/v/ffm.yml)
 
+CONFIGS_V=(configs/pomdp/hopper_blt/v/ffm.yml
+	configs/pomdp/walker_blt/v/ffm.yml
+	configs/pomdp/ant_blt/v/ffm.yml
+	configs/pomdp/cheetah_blt/v/ffm.yml
+	configs/pomdp/hopper_blt/v/rnn.yml
+	configs/pomdp/walker_blt/v/rnn.yml
+	configs/pomdp/ant_blt/v/rnn.yml
+	configs/pomdp/cheetah_blt/v/rnn.yml)
+
+CONFIGS_P=(configs/pomdp/ant_blt/p/ffm.yml
+	configs/pomdp/cheetah_blt/p/ffm.yml
+	configs/pomdp/hopper_blt/p/ffm.yml
+	configs/pomdp/walker_blt/p/ffm.yml
+	configs/pomdp/ant_blt/p/rnn.yml
+	configs/pomdp/cheetah_blt/p/rnn.yml
+	configs/pomdp/hopper_blt/p/rnn.yml
+	configs/pomdp/walker_blt/p/rnn.yml)
+
 SEED=$1
 CUDA=$2
 declare -n CONFIGS=$3
